4123400fe80a8dafd244d43cfcea3e31fa478dcf
adamz <58021562+ad4mny@users.noreply.github.com>
2021-12-28 22:53:04 +0800

    url rename configphp

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/source-code-anaysis-tool/';
+$config['base_url'] = 'http://localhost/source-code-analysis-tool/';
 
 /*
 |--------------------------------------------------------------------------
